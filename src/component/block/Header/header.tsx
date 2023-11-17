be50ee88e047cc5a10f0480ddd19f436306da7d5
SEIMIN-BJJ <interkq12@gmail.com>
2023-11-17 16:24:20 +0900

    .

--- src/component/block/Header/header.tsx
+++ src/component/block/Header/header.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { FaGithub } from "react-icons/fa6";
+import { FiMail } from "react-icons/fi";
+import { FaYoutube } from "react-icons/fa";
 
 const Header = styled.header`
   width: 100%;
@@ -7,14 +10,14 @@ const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  border: 1px solid black;
   position: fixed;
   left: 0;
   top: 0;
   right: 0;
-  background-color: salmon;
+  background-color: #467ba1db;
+  z-index: 100;
   transition: transform 0.3s ease-in-out; 
-
+  opacity: 0.5;
   &.hidden {
     transform: translateY(-100%); 
   }
@@ -33,13 +36,14 @@ const Header = styled.header`
 `;
 
 const Logo = styled.h4`
-  width: 5rem;
+  width: 100%;
   height: 100%;
-  border: 1px solid black;
   margin: 0 12.5rem;
   display: flex;
   justify-content: flex-start;
   align-items: center;
+  color:#fff;
+  font-family: "Pretendard-Bold";
 
   @media (max-width: 768px) {
     width: 100%;
@@ -48,10 +52,9 @@ const Logo = styled.h4`
 `;
 
 const Nav = styled.nav`
-  width: 200px;
+  width: 30%;
   height: 100%;
-  border: 1px solid black;
-  margin: 0 12.5rem;
+  margin: 0 12rem;
 
   @media (max-width: 768px) {
     width: 100%;
@@ -62,22 +65,22 @@ const Ul = styled.ul`
   width: 100%;
   display: flex;
   justify-content: space-between;
-  border: 1px solid black;
+
 `;
 
 const Li = styled.li`
   width: 100%;
   height: 3.3rem;
-  font-size: 15px;
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid black;
 
   a {
+    color:#fff;
     display: flex;
     justify-content: center;
     align-items: center;
+    font-size: 1.3rem;
 
     @media (max-width: 768px) {
       width: 100%;
@@ -109,13 +112,13 @@ const HeaderComp = () => {
       <Nav>
         <Ul>
           <Li>
-            <a href="/">github</a>
+            <a href="/"><FaGithub /></a>
           </Li>
           <Li>
-            <a href="/">mail</a>
+            <a href="/"><FiMail /></a>
           </Li>
           <Li>
-            <a href="/">youtube</a>
+            <a href="/"><FaYoutube /></a>
           </Li>
         </Ul>
       </Nav>
