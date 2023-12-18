1dadd45c8c6f7436bfeeffda24b92990631ac1fa
SEIMIN-BJJ <interkq12@gmail.com>
2023-12-18 18:10:10 +0900

    .

--- src/component/pages/ThirdSection.tsx
+++ src/component/pages/ThirdSection.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { motion, AnimatePresence } from "framer-motion";
-import ImacIMG from "../assets/images/pngwing.com.png"
+import ImacIMG from "../assets/images/pngwing.com.png";
+import NovelIMGFirst from "../assets/images/novel-img-1.png";
 import "../../App.scss";
 
 const Container = styled.section`
@@ -133,25 +134,30 @@ const ThirdBoxSub = styled(motion.section)`
       .Sn-Page {
       width: 100%;
       height: 100%;
-      color: #ccc;
+      color: #b2b2b2;
       font-size: 2rem;
-      font-family: "ChosunCentennial";
       background-color: #191919;
+      font-family: "ChosunCentennial";
       transition: 1s ease-in-out ;
       cursor: pointer;
 
       &:hover {
-      border: 2.5px solid #ffffff; 
-      transition: border 0.1s ease-in-out; 
-
+        color:#fff;
+        text-shadow:
+          0 0 30px #000,
+          0 0 60px #b23d39,
+          0 0 70px #b23d39,
+          0 0 90px #b23d39;
+        opacity: 1;
+      }
+      
       @media (max-width: 768px) {
       width: 100%;
-      height: 15vh;
+      height: 10vh;
       display: flex;
       justify-content: center;
       align-items: center;
       }
-    }
   }
 }
 `;
@@ -166,7 +172,7 @@ const ModalBackground = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 100;
+  z-index: 1;
   cursor: pointer; 
 
 `;
@@ -176,40 +182,151 @@ const ModalContent = styled(motion.div)`
   height: 100%;
   max-width: 80%;
   max-height: 80%;
-  background-color: #ffffff;
+  background-color: #171717;
   padding: 20px;
   border-radius: 8px;
   overflow-y: auto;
+  color: #fff;
 
   article {
     width: 100%;
     height: 100%;
-    border: 1px solid #ccc;
     border-radius: 8px;
     display: flex;
     justify-content: center;
     align-items: center;
 
     ul {
-      width: 50%;
+      width: 100%;
       height: 100%;
       display: flex;
       justify-content: center;
       align-items: center;
-      border: 1px solid black;
+      flex-direction: column;
 
       .Monitor {
       width: 100%;
-      height: 80%;
+      height: 75%;
       background-image: url(${ImacIMG});
       background-position: center center;
       background-repeat: no-repeat;
       background-size: contain;
 
+      p {
+        width: 80%;
+        height: 62%;
+        background-image: url(${NovelIMGFirst});
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center center;
+        margin: 1rem auto;
+      }
+        }
+
+      .MonitorTitle {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 3rem;
+        font-family: "ChosunCentennial";
+
+      }
+
+      .MonitorText {
+        width: 100%;
+        height: auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: "ChosunCentennial";
+        font-size: 1rem;
+
+        p {
+        width: 100%;
+        height: 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px 20px;
+        margin-bottom: 3rem;
+        }
+      }
+
+      .MonitorFooter {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: "ChosunCentennial";
+        font-size: 1.2rem;
+        flex-direction: column;
+
+        p {
+        width: 100%;
+        height: 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px 20px;
+        }
+
+        div {
+          width: 100%;
+          height: 10vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin:1rem auto;
+
+        button {
+          width: 5rem;
+          height: 2rem;
+          border: 1px solid white;
+          padding: 0.5rem auto;
+          margin: 0.3rem;
+          font-family: "ChosunCentennial";
+          z-index: 999;
+          border-radius: 50px;
+          a {
+          color: #fff;
+        }
+          &:hover {
+          color: #171717;
+          background-color: #fff;
+
+          a {
+            color: #171717;
+            background-color: #fff;
+          }
+        }
+          }
+        }
+      }
       }
     }
-  }
 `;
+
+const MonitorDiscriptions = [
+  {
+    title: (
+      <p>
+        레트로 게임의 장르 중 하나로 BGM과 소설을 합쳐 사운드 노벨이라 칭한다.
+        <br />
+        글의 내용과 어울리는 사운드를 접목하여 책을 읽듯이 BGM을 통하여
+        <br />내용의 분위기를 글로만 읽는 것과 다르게 긴장감을 극대화 한다.<br />또한, 말그대로 노벨이기 때문에 기술적인 목적보단 <br />글의 내용과 사운드에 집중을 하였다. 
+      </p>
+    ),
+
+    text: (
+      <p>
+        제작기간 : 1일 / 기여도: 100% 
+      </p>
+    )
+  },
+];
 
 const ThirdSection = () => {
 
@@ -333,16 +450,47 @@ const ThirdSection = () => {
               transition={transition}>
                 <article>
                   <ul>
-                    <li className="Monitor"></li>
+                    <li className="Monitor">
+                      <p></p>
+                    </li>
                   </ul>
-                  <ul></ul>
+                    <ul>
+                      <li className="MonitorTitle">SOUND NOVEL</li>
+                      <ul>
+                        {MonitorDiscriptions.map((item, index) => (
+                        <li className="MonitorText" key={index}>
+                          <p>{item.title}</p>
+                        </li>
+                        ))}
+                      </ul>
+                      <ul>
+                        {MonitorDiscriptions.map((item,index) => (
+                      <li className="MonitorFooter" key={index}>
+                        <p>{item.text}</p>
+                        <div>
+                          <button>
+                            <a 
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              href="https://github.com/SEIMIN-BJJ/Sound-Novel-react">Github</a>
+                            </button>
+                            <button>
+                            <a 
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              href="https://seimin-bjj.github.io/Sound-Novel-react/">Web</a>
+                            </button>
+                        </div>
+                      </li>
+                      ))}
+                      </ul>
+                    </ul>
                 </article>
               </ModalContent>
             </ModalBackground>
           )}
         </AnimatePresence>
       </Content>
-
     </Container>
   );
 };
